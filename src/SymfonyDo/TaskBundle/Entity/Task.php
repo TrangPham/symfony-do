2f22570e4f0da030f83109c73b828255b521bc07
Thu Trang Pham <thuutrangpham@gmail.com>
2014-08-22 13:41:25 -0400

    Doctrine sqlite set-up

--- src/SymfonyDo/TaskBundle/Entity/Task.php
+++ src/SymfonyDo/TaskBundle/Entity/Task.php
@@ -1,8 +1,62 @@
-namespace SymfonyDo/TaskBundle/Entity;
+<?php
 
+namespace SymfonyDo\TaskBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Task
+ * @ORM\Entity
+ * @ORM\Table(name="task")
+ */
 class Task
 {
-	protected $userid;
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
-	protected $description;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Task
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
 }
